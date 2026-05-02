3297835c6a31791072bb4f50b724f24f0f3c8606
epsilon <jiraiyahiddenleaf27@gmail.com>
2026-05-03 00:18:31 +0530

    update rss feed

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -5,17 +5,18 @@ import { SITE } from "../consts";
 
 export const GET: APIRoute = async (context) => {
   const posts = (await getCollection("blog", ({ data }) => !data.draft)).sort(
-    (a, b) => b.data.publishDate.getTime() - a.data.publishDate.getTime(),
+    (a, b) => b.data.pubDate.getTime() - a.data.pubDate.getTime(),
   );
 
   return rss({
     title: SITE.title,
-    description: SITE.description,
-    site: context.site ?? SITE.siteUrl,
+    description: SITE.desc,
+    site: SITE.siteUrl,
     items: posts.map((post) => ({
       title: post.data.title,
-      description: post.data.description,
-      pubDate: post.data.publishDate,
+      description: post.data.desc,
+      pubDate: post.data.pubDate,
+      categories: post.data.tags,
       link: `/blog/${post.id}/`,
     })),
     customData: `<language>${SITE.language}</language>`,
